82233246551c2af09a82a2a66d705eeae6db217f
Gabriel <gabrielmaranhao.cpm@gmail.com>
2025-01-29 17:37:55 -0300

    feat: Avatars List Componenet + currencyConverter function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,34 @@
-import { CardComponent, Header } from '@/components'
+import { AvatarList, CardComponent, Header } from '@/components'
+import { currencyConverter } from '@/utils'
 import { Container } from '@mui/material'
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Gabriel Maranhão',
+      subtitle: currencyConverter(1000),
+    },
+    {
+      avatar: '/dnc-avatar.jpg',
+      name: 'Lore Ipsum',
+      subtitle: currencyConverter(3300.9),
+    },
+    {
+      avatar: '/dnc-avatar.jpg',
+      name: 'Donald Glover',
+      subtitle: currencyConverter(7000),
+    },
+  ]
+
   return (
     <>
       <Header />
       <Container maxWidth="lg">
         <CardComponent className="">CARD</CardComponent>
+        <CardComponent>
+          <AvatarList listData={mockListData}></AvatarList>
+        </CardComponent>
       </Container>
       <h1>HOME</h1>
     </>
