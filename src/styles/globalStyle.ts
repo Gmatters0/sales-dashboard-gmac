b1e9ae3b1ba0217eefea7c0198353d82d9524720
Gabriel <gabrielmaranhao.cpm@gmail.com>
2025-02-10 15:03:59 -0300

    feat: Logout flow

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,3 +1,4 @@
+import { pxToRem } from '@/utils'
 import { createGlobalStyle } from 'styled-components'
 import { DefaultTheme } from 'styled-components'
 
@@ -13,5 +14,11 @@ export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
   h1, h2, h3, p, ul, li, figure {
     margin: 0;
     padding: 0;
+  }
+  .mb-1 {
+    margin-bottom: ${pxToRem(16)};
+  }
+  .mb-2 {
+    margin-bottom: ${pxToRem(32)};
   }
 `
